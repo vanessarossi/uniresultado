1d892c7843d1f2dc8959f124f536f7b7879fd471
Vanessa Rossi <vanessa_rossi@outlook.com>
2019-05-10 18:02:47 -0300

    teste de procedure

--- src/br/coop/unimedriopardo/uniresultado/repository/RepositorioResultado.java
+++ src/br/coop/unimedriopardo/uniresultado/repository/RepositorioResultado.java
@@ -16,11 +16,11 @@ public interface RepositorioResultado extends JpaRepository<Resultado, Integer> 
 	public Page<Resultado> findByPrestador_idAndStatus(Integer id, String status, Pageable pageable);
 	public Page<Resultado> findByPrestador_idOrderByIdDesc(Integer id, Pageable pageable);
 	
-	@Query(value = "{CALL PROC_LIBERAR_ERRO_VALIDACAO(:prestador_id)}", nativeQuery = true)
-    public Object liberarErroValidacao(@Param("prestador_id")Integer prestador_id);
+	@Query(value = "{EXECUTE PROC_LIBERAR_ERRO_VALIDACAO(:prestador_id)}", nativeQuery = true)
+    public void liberarErroValidacao(@Param("prestador_id")Integer prestador_id);
 	
-	@Query(value = "{CALL PROC_VALIDAR_EXAME_PRESTADOR(:prestador_id)}", nativeQuery = true)
-    public Object validarExames(@Param("prestador_id")Integer prestador_id);
+	@Query(value = "{EXECUTE PROC_VALIDAR_EXAME_PRESTADOR(:prestador_id)}", nativeQuery = true)
+    public void validarExames(@Param("prestador_id")Integer prestador_id);
 }
 
 
